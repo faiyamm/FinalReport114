b98633cffcddb0cbbf6f4c3b3d7cce0dd9a8931d
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-04-24 12:06:45 -0700

    Initial Commit

---
+++ FinalReport114/FinalReport114App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalReport114App.swift
+//  FinalReport114
+//
+//  Created by Fai on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalReport114App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
